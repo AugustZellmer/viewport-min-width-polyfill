18d4f5b1e1d9d1be618220f562fa273a9f460481
August Zellmer <AugustZellmer@users.noreply.github.com>
2020-02-16 16:26:18 -0600

    rewrote existing functionality, but now the other parameters in the content are preserved. only "width" is overwritten.

--- viewport-min-width.js
+++ viewport-min-width.js
@@ -10,30 +10,72 @@
  *                      is that Firefox doesn't pick up changes to the viewport
  *                      meta tag.
  *
- * Author: Brendan Long <self@brendanlong.com>
+ * Author: Brendan Long <self@brendanlong.com> & August Zellmer
  * License: Public Domain - http://unlicense.org/
  * See: https://github.com/brendanlong/viewport-min-width-polyfill
  */
-(function() {
-  var viewport = document.querySelector("meta[name=viewport]");
-  if (viewport) {
-    var content = viewport.getAttribute("content");
-    var parts = content.split(",");
-    for (var i = 0; i < parts.length; ++i) {
-      var part = parts[i].trim();
-      var pair = part.split("=");
-      if (pair[0] === "min-width") {
-        var minWidth = parseInt(pair[1]);
-        if (screen.width < minWidth) {
-          document.head.removeChild(viewport);
+ 
+var initialViewport = null;
+var minWidth = null;
 
-          var newViewport = document.createElement("meta");
-          newViewport.setAttribute("name", "viewport");
-          newViewport.setAttribute("content", "width=" + minWidth);
-          document.head.appendChild(newViewport);
-          break;
-        }
-      }
-    }
-  }
-})();
+initMinViewportWidth();
+
+function initMinViewportWidth(){
+	initialViewport = document.querySelector("meta[name=viewport]");
+	if (!initialViewport) {
+		console.warn("No meta[name=viewport] element found. Minimum viewport width cannot be set. Please ensure that the meta[name=viewport] element has been loaded before function updateMetaViewport() is called.");
+		return;
+	}
+	
+	try{
+		minWidth = getMinWidth(initialViewport);
+	}
+	catch(const s){
+		console.warn(s);
+		return;
+	}
+}
+ 
+function updateMetaViewport() {
+	if(!initialViewport || !minWidth){
+		console.warn("Function initMinViewportWidth() has not been called. Minimum viewport width cannot be set. Please ensure that function initMinViewportWidth() is called before function updateMetaViewport().");
+	}
+	
+	if (screen.width < minWidth) {
+		const newViewport = initialViewport;
+		
+		const content = initialViewport.getAttribute("content");
+		content = replaceWidth(content, minWidth);
+		newViewport.setAttribute("content", content);
+		
+		document.head.removeChild(initialViewport);
+		document.head.appendChild(newViewport);
+	}
+}
+
+function getMinWidth(initialViewport){
+	const content = initialViewport.getAttribute("content");
+	const parts = content.split(",");
+	for (var i = 0; i < parts.length; ++i) {
+		const part = parts[i].trim();
+		const pair = part.split("=");
+		if (pair[0] === "min-width") {
+			const minWidth parseInt(pair[1]);
+			if(!isNaN(minWidth){
+				return minWidth;
+			}
+			throw "Minimum viewport width cannot be set to a non-numeric value.";
+		}
+	}
+	throw "No min-width property found in the content of the meta[name=viewport] element. Minimum viewport width cannot be set.";
+}
+
+function replaceWidth(content, minWidth){
+	const regex = /width *= *.*?,/;
+	const newWidth = "width=".concat(minWidth);
+	const newContent = content.replace(regex, newWidth);
+	return newContent;
+}
+	
+	
+	
